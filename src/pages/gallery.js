eaeb1f550a40d597711043db766c6fd7b1f0e118
sam9111 <samyuktha19095@cse.ssn.edu.in>
2021-08-03 17:53:20 +0530

    rename gallery to yearbook

--- src/pages/gallery.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Box, Button } from "@chakra-ui/react";
-
-function GalleryPage() {
-  return <Box>Gallery</Box>;
-}
-
-export default GalleryPage;
---
+++ src/pages/yearbook.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Box, Button } from "@chakra-ui/react";
+
+function YearBookPage() {
+  return <Box>YearBook</Box>;
+}
+
+export default YearBookPage;
