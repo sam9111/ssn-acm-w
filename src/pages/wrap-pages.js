36b690683b0836b0cb3bd17ad523a4207dde3f80
AmanRK <amankhemka.ak28@gmail.com>
2021-08-05 13:03:33 +0530

    merge conflict fix

--- src/pages/wrap-pages.js
+++ src/pages/wrap-pages.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../components/Layout.js';
+import Layout from '../components/Layout';
 
 export function wrapPages({ element }) {
   return <Layout>{element}</Layout>;
