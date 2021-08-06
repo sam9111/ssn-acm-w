3c2f144a6822f02754e866b64d151f0ba5bf6fd7
Keerthana <keerthana19132@cse.ssn.edu.in>
2021-08-06 16:37:39 +0530

    team page

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { Box, Button } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react"
 
 function TeamPage() {
-  return <Box>Team</Box>;
+  return <Box>     
+    <Box w="338px" h="72px" left="551px" top="164xl" bgGradient="linear(to-t, green.200, pink.500)">
+    <Text fontSize="72px" Align="center" fontWeight="bold" lineHeight="72px" > Our Team</Text>  
+    </Box>
+
+    <Box w="255px" h="29px" mr="429px" mt="432px" bgGradient="linear(to-t, green.200, pink.500)">
+    <Text fontSize="36px" Align="center" fontWeight="normal" lineHeight="36px"> Dr. Chitra Babu</Text>
+    </Box>
+
+    <Box w="297px" h="27px" ml="727px" mt="434xl" bgGradient="linear(to-t, green.200, pink.500)">
+    <Text fontSize="36px" Align="center" fontWeight="normal" lineHeight="36px"> Dr. K. Madheswari</Text>
+    </Box>
+   </Box>;
 }
 
 export default TeamPage;
