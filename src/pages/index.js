a1cdc9d33cd3f4d2c21005e17798596242f63ee6
sam9111 <samyuktha19095@cse.ssn.edu.in>
2021-08-17 10:50:40 +0530

    added card component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,9 @@
 // src/pages/index.js
 import React from "react";
-import {
-  Container,
-  Stack,
-  Flex,
-  Box,
-  Heading,
-  Text,
-  Button,
-  Img,
-  Icon,
-  IconButton,
-  createIcon,
-  IconProps,
-  useColorModeValue,
-  Center,
-} from "@chakra-ui/react";
+import { Container, Stack, Box, Heading, Text, Img } from "@chakra-ui/react";
 import Saly_19 from "../images/Saly-19.png";
+
+import HomeCard from "../components/HomeCard.js";
 
 function IndexPage() {
   return (
@@ -63,213 +50,19 @@ function IndexPage() {
           >
             Creating an inclusive environment for women in computing
           </Text>
-          <Flex
-            flex={1}
-            justify={"center"}
-            align={"center"}
-            position={"relative"}
-            w={"full"}
-          >
-            <Center
-              bg={useColorModeValue("white", "gray.800")}
-              position={"relative"}
-              borderRadius={"3xl"}
-              boxShadow={"lg"}
-              width={"full"}
-              overflow={"hidden"}
-              p={10}
-            >
-              <Text
-                lineHeight={1.1}
-                fontWeight={300}
-                fontSize={{ base: "sm", sm: "md", lg: "lg" }}
-                as={"span"}
-                position={"relative"}
-              >
-                ACM-W SSN is a team of enthusiastic engineers representing the
-                diverse community of Association of Computing Machinery’s
-                Council of Women in Computing (ACM - W), with a student chapter
-                based out of SSN College of Engineering, Chennai. We work
-                towards helping computing professionals to advance in their
-                profession by forming connections, skill-building and creating a
-                positive social impact.{" "}
-              </Text>
-            </Center>
-          </Flex>
+          <HomeCard />
         </Stack>
       </Stack>
-      <Flex
-        flex={1}
-        justify={"center"}
-        align={"center"}
-        position={"relative"}
-        w={"full"}
-      >
-        <Center
-          bg={useColorModeValue("white", "gray.800")}
-          position={"relative"}
-          borderRadius={"3xl"}
-          boxShadow={"lg"}
-          width={"full"}
-          overflow={"hidden"}
-          p={10}
-        >
-          <Text
-            lineHeight={1.1}
-            fontWeight={300}
-            fontSize={{ base: "sm", sm: "md", lg: "lg" }}
-            as={"span"}
-            position={"relative"}
-          >
-            ACM-W SSN is a team of enthusiastic engineers representing the
-            diverse community of Association of Computing Machinery’s Council of
-            Women in Computing (ACM - W), with a student chapter based out of
-            SSN College of Engineering, Chennai. We work towards helping
-            computing professionals to advance in their profession by forming
-            connections, skill-building and creating a positive social
-            impact.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Congue turpis lacus amet eget. Elit a, ut viverra enim. Pellentesque
-            scelerisque risus a, consequat cursus consequat. Ullamcorper felis
-            cursus morbi quis faucibus volutpat malesuada elementum habitant.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Congue
-            turpis lacus amet eget. Elit a, ut viverra enim. Pellentesque
-            scelerisque risus a, consequat cursus consequat. Ullamcorper felis
-            cursus morbi quis faucibus volutpat malesuada elementum habitant.
-          </Text>
-        </Center>
-      </Flex>
-
+      <HomeCard />
       <Stack
         align={"center"}
         spacing={{ base: 8, md: 10 }}
         py={{ base: 20, md: 28 }}
         direction={{ base: "column", md: "row" }}
       >
-        <Flex
-          flex={1}
-          justify={"center"}
-          align={"center"}
-          position={"relative"}
-          w={"full"}
-        >
-          <Center
-            bg={useColorModeValue("white", "gray.800")}
-            position={"relative"}
-            borderRadius={"3xl"}
-            boxShadow={"lg"}
-            width={"full"}
-            overflow={"hidden"}
-            p={10}
-          >
-            <Text
-              lineHeight={1.1}
-              fontWeight={300}
-              fontSize={{ base: "sm", sm: "md", lg: "lg" }}
-              as={"span"}
-              position={"relative"}
-            >
-              ACM-W SSN is a team of enthusiastic engineers representing the
-              diverse community of Association of Computing Machinery’s Council
-              of Women in Computing (ACM - W), with a student chapter based out
-              of SSN College of Engineering, Chennai. We work towards helping
-              computing professionals to advance in their profession by forming
-              connections, skill-building and creating a positive social
-              impact.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Congue turpis lacus amet eget. Elit a, ut viverra enim.
-              Pellentesque scelerisque risus a, consequat cursus consequat.
-              Ullamcorper felis cursus morbi quis faucibus volutpat malesuada
-              elementum habitant. Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Congue turpis lacus amet eget. Elit a, ut viverra
-              enim. Pellentesque scelerisque risus a, consequat cursus
-              consequat. Ullamcorper felis cursus morbi quis faucibus volutpat
-              malesuada elementum habitant.
-            </Text>
-          </Center>
-        </Flex>
-
-        <Flex
-          flex={1}
-          justify={"center"}
-          align={"center"}
-          position={"relative"}
-          w={"full"}
-        >
-          <Center
-            bg={useColorModeValue("white", "gray.800")}
-            position={"relative"}
-            borderRadius={"3xl"}
-            boxShadow={"lg"}
-            width={"full"}
-            overflow={"hidden"}
-            p={10}
-          >
-            <Text
-              lineHeight={1.1}
-              fontWeight={300}
-              fontSize={{ base: "sm", sm: "md", lg: "lg" }}
-              as={"span"}
-              position={"relative"}
-            >
-              ACM-W SSN is a team of enthusiastic engineers representing the
-              diverse community of Association of Computing Machinery’s Council
-              of Women in Computing (ACM - W), with a student chapter based out
-              of SSN College of Engineering, Chennai. We work towards helping
-              computing professionals to advance in their profession by forming
-              connections, skill-building and creating a positive social
-              impact.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Congue turpis lacus amet eget. Elit a, ut viverra enim.
-              Pellentesque scelerisque risus a, consequat cursus consequat.
-              Ullamcorper felis cursus morbi quis faucibus volutpat malesuada
-              elementum habitant. Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Congue turpis lacus amet eget. Elit a, ut viverra
-              enim. Pellentesque scelerisque risus a, consequat cursus
-              consequat. Ullamcorper felis cursus morbi quis faucibus volutpat
-              malesuada elementum habitant.
-            </Text>
-          </Center>
-        </Flex>
-
-        <Flex
-          flex={1}
-          justify={"center"}
-          align={"center"}
-          position={"relative"}
-          w={"full"}
-        >
-          <Center
-            bg={useColorModeValue("white", "gray.800")}
-            position={"relative"}
-            borderRadius={"3xl"}
-            boxShadow={"lg"}
-            width={"full"}
-            overflow={"hidden"}
-            p={10}
-          >
-            <Text
-              lineHeight={1.1}
-              fontWeight={300}
-              fontSize={{ base: "sm", sm: "md", lg: "lg" }}
-              as={"span"}
-              position={"relative"}
-            >
-              ACM-W SSN is a team of enthusiastic engineers representing the
-              diverse community of Association of Computing Machinery’s Council
-              of Women in Computing (ACM - W), with a student chapter based out
-              of SSN College of Engineering, Chennai. We work towards helping
-              computing professionals to advance in their profession by forming
-              connections, skill-building and creating a positive social
-              impact.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Congue turpis lacus amet eget. Elit a, ut viverra enim.
-              Pellentesque scelerisque risus a, consequat cursus consequat.
-              Ullamcorper felis cursus morbi quis faucibus volutpat malesuada
-              elementum habitant. Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Congue turpis lacus amet eget. Elit a, ut viverra
-              enim. Pellentesque scelerisque risus a, consequat cursus
-              consequat. Ullamcorper felis cursus morbi quis faucibus volutpat
-              malesuada elementum habitant.
-            </Text>
-          </Center>
-        </Flex>
+        <HomeCard />
+        <HomeCard />
+        <HomeCard />
       </Stack>
     </Container>
   );
