405c9f3733fdaa282f569daa8652eda809ec4dd4
sam9111 <samyuktha19095@cse.ssn.edu.in>
2021-08-18 20:37:43 +0530

    config changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,15 @@ module.exports = {
     "gatsby-plugin-gatsby-cloud",
     "@chakra-ui/gatsby-plugin",
     "gatsby-plugin-wrap-pages",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-graphcms",
       options: {
         endpoint:
           "https://api-ap-northeast-1.graphcms.com/v2/cksfnx5ig0n6l01xvcjga06wr/master",
+        downloadLocalImages: true,
       },
     },
   ],
