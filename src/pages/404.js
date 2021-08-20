692e6aa3b350a1c5be685f8912089bd0efae6833
Keerthana <keerthana19132@cse.ssn.edu.in>
2021-08-20 14:13:36 +0530

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,17 @@
 import * as React from "react";
-import { Link } from "gatsby";
-
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-};
-
-// markup
-const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  );
-};
-
+import { Link ,Img} from "gatsby";
+import Saly_17 from "../images/Saly-17.png";
+function TeamPage() {
+return  ( <Box>
+<Box position={"relative"} overflow={"hidden"} w={[300, 400, 500]}>
+          <Img
+            alt={"Wanderer"}
+            fit={"cover"}
+            align={"center"}
+            src={Saly_17}
+          />
+        </Box>
+</Box>
+);
+}
 export default NotFoundPage;
