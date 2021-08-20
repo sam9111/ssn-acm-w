b98cc70278681b81d11755ab130bfb8263440d23
Keerthana <keerthana19132@cse.ssn.edu.in>
2021-08-20 15:40:11 +0530

    404-done

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,40 @@
 import * as React from "react";
-import { Link ,Img} from "gatsby";
+import {Img,Box,Center,Text,Container,Button} from "@chakra-ui/react";
+import {Link} from "gatsby";
+;
 import Saly_17 from "../images/Saly-17.png";
-function TeamPage() {
+function NotFoundPage() {
 return  ( <Box>
-<Box position={"relative"} overflow={"hidden"} w={[300, 400, 500]}>
+<Center h="25em"><Text
+        fontWeight={"extrabold"}
+        fontSize={{ base: "lg", sm: "6em", lg: "15em" }}
+        >404</Text>
+        </Center>
+        <Center h="0.1em">
+        <Text fontWeight={"bold"}
+        fontSize={{base: "lg" , sm: "3em" , lg:"5em"}}
+        >Oops!</Text>
+        </Center>
+        <Center h="10em">
+        <Text fontWeight={"normal"}
+        fontSize={{base: "lg" , sm: "2em" , lg:"3em"}}
+        >Hey there! Looks like you wandered and got lost!</Text>
+        </Center>
+        <Center >
+          <Button size="lg" height="3em" colorScheme="#000000">
+      <Link to="/"><Text fontSize={{base: "lg" , sm: "1em" , lg:"2em"}}
+        >Go Back!</Text></Link>
+      </Button>
+    </Center>
+        <Center>
           <Img
             alt={"Wanderer"}
             fit={"cover"}
             align={"center"}
             src={Saly_17}
           />
-        </Box>
+          
+        </Center>
 </Box>
 );
 }
