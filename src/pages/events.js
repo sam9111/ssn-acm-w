6b340ec103962e2ef5c805cd3e3b67feb8e32cc6
sam9111 <samyuktha19095@cse.ssn.edu.in>
2021-08-24 10:23:22 +0530

    events basic layout

--- src/pages/events.js
+++ src/pages/events.js
@@ -2,47 +2,64 @@
 import React from "react";
 import { Center, Heading } from "@chakra-ui/react";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Flex } from "@chakra-ui/react";
 function Events() {
   return (
     <Box>
-      <Heading textAlign="center" padding="16">
-        Events
-      </Heading>
-      <Center>
-        <Tabs variant="line" size="lg">
-          <TabList>
-            <Tab>
-              <Text fontSize="2xl" fontWeight="bold">
-                Past
-              </Text>
-            </Tab>
-            <Tab>
-              <Text fontSize="2xl" fontWeight="bold">
-                2020-2021
-              </Text>
-            </Tab>
-            <Tab>
-              <Text fontSize="2xl" fontWeight="bold">
-                Upcoming
-              </Text>
-            </Tab>
-          </TabList>
-          <br></br>
-          <TabPanels>
-            <TabPanel>
-              <p style={{ align: "left" }}>Past</p>
-            </TabPanel>
-            <TabPanel>
-              <div>
-              </div>
-            </TabPanel>
-            <TabPanel>
-              <p>Upcoming</p>
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </Center>
+      <Box py={{ base: 10, md: 14 }} align={"center"}>
+        <Heading
+          lineHeight={1.1}
+          fontWeight={600}
+          fontSize={{ base: "3xl", sm: "4xl", lg: "5xl" }}
+        >
+          <Text as={"span"} position={"relative"}>
+            Events
+          </Text>
+        </Heading>
+        <Box width={"full"} p={10}>
+          <Tabs variant="line" align="center" isFitted>
+            <TabList>
+              <Tab>
+                <Text
+                  fontSize={{ base: "lg", sm: "xl", lg: "2xl" }}
+                  fontWeight="bold"
+                >
+                  Past
+                </Text>
+              </Tab>
+              <Tab>
+                <Text
+                  fontSize={{ base: "lg", sm: "xl", lg: "2xl" }}
+                  fontWeight="bold"
+                >
+                  {new Date().getFullYear() +
+                    " - " +
+                    (new Date().getFullYear() + 1)}
+                </Text>
+              </Tab>
+              <Tab>
+                <Text
+                  fontSize={{ base: "lg", sm: "xl", lg: "2xl" }}
+                  fontWeight="bold"
+                >
+                  Upcoming
+                </Text>
+              </Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <p style={{ align: "left" }}>Past</p>
+              </TabPanel>
+              <TabPanel>
+                <div></div>
+              </TabPanel>
+              <TabPanel>
+                <p>Upcoming</p>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
+      </Box>
     </Box>
   );
 }
