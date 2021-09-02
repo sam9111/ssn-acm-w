d6a6301ab7d24e9e5768db2a004634afc73cbd9d
ramumaha <ramumaha123@gmail.com>
2021-09-02 09:47:58 +0530

    Added the graphql part
    Doesnt work as expected

--- src/components/CommonCardLayout.js
+++ src/components/CommonCardLayout.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, useProps } from "@chakra-ui/react";
 import careertalk from "../images/careertalk.png";
 import CommonCard from "../components/CommonCard.js";
 
-export default function CommonCardLayout() {
+export default function CommonCardLayout(props) {
+  // const entries = Object.entries(props);
+  const value = Object.values(props);
+  console.log(value);
+  const temp = value[0];
+
   return (
     <SimpleGrid spacing="40px" columns={{ base: 1, md: 2 }} width={"full"}>
+<<<<<<< Updated upstream
       <CommonCard
         title=" Coding Essentials"
         imageImport={careertalk}
@@ -41,6 +47,17 @@ export default function CommonCardLayout() {
         link="Check it Out!"
         cardType="events"
       />
+=======
+      {temp.map((eventElement) => (
+        <CommonCard
+          title={eventElement.title}
+          imageImport={careertalk}
+          body={eventElement.about}
+          link="www.google.com"
+          cardType="event"
+        />
+      ))}
+>>>>>>> Stashed changes
     </SimpleGrid>
   );
 }
--- src/pages/events.js
+++ src/pages/events.js
@@ -11,65 +11,84 @@ import {
   Text,
   Flex,
 } from "@chakra-ui/react";
+import { graphql, StaticQuery } from "gatsby";
 
 import CommonCardLayout from "../components/CommonCardLayout";
 
 function Events() {
   return (
-    <Box>
-      <Box py={{ base: 10, md: 14 }} align={"center"}>
-        <Heading
-          lineHeight={1.1}
-          fontWeight={600}
-          fontSize={{ base: "3xl", sm: "4xl", lg: "5xl" }}
-        >
-          <Text as={"span"} position={"relative"}>
-            Events
-          </Text>
-        </Heading>
-        <Tabs variant="line" align="center" isFitted width={"full"} p={10}>
-          <TabList>
-            <Tab>
-              <Text
-                fontSize={{ base: "lg", sm: "xl", lg: "2xl" }}
-                fontWeight="bold"
-              >
-                Past
+    <StaticQuery
+      query={graphql`
+        {
+          allGraphCmsEvent {
+            nodes {
+              title
+              about
+              when
+              coverImage {
+                url
+              }
+            }
+          }
+        }
+      `}
+      render={(data) => (
+        <Box>
+          <Box py={{ base: 10, md: 14 }} align={"center"}>
+            <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: "3xl", sm: "4xl", lg: "5xl" }}
+            >
+              <Text as={"span"} position={"relative"}>
+                Events
               </Text>
-            </Tab>
-            <Tab>
-              <Text
-                fontSize={{ base: "lg", sm: "xl", lg: "2xl" }}
-                fontWeight="bold"
-              >
-                {new Date().getFullYear() +
-                  " - " +
-                  (new Date().getFullYear() + 1)}
-              </Text>
-            </Tab>
-            <Tab>
-              <Text
-                fontSize={{ base: "lg", sm: "xl", lg: "2xl" }}
-                fontWeight="bold"
-              >
-                Upcoming
-              </Text>
-            </Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <CommonCardLayout />
-            </TabPanel>
-            <TabPanel>
-              <CommonCardLayout />
-            </TabPanel>
-            <TabPanel>
-              <CommonCardLayout />
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </Box>
-    </Box>
+            </Heading>
+            <Tabs variant="line" align="center" isFitted width={"full"} p={10}>
+              <TabList>
+                <Tab>
+                  <Text
+                    fontSize={{ base: "lg", sm: "xl", lg: "2xl" }}
+                    fontWeight="bold"
+                  >
+                    Past
+                  </Text>
+                </Tab>
+                <Tab>
+                  <Text
+                    fontSize={{ base: "lg", sm: "xl", lg: "2xl" }}
+                    fontWeight="bold"
+                  >
+                    {new Date().getFullYear() +
+                      " - " +
+                      (new Date().getFullYear() + 1)}
+                  </Text>
+                </Tab>
+                <Tab>
+                  <Text
+                    fontSize={{ base: "lg", sm: "xl", lg: "2xl" }}
+                    fontWeight="bold"
+                  >
+                    Upcoming
+                  </Text>
+                </Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel>
+                  <CommonCardLayout props={data.allGraphCmsEvent.nodes} />
+                </TabPanel>
+                <TabPanel>
+                  <CommonCardLayout props={data.allGraphCmsEvent.nodes} />
+                </TabPanel>
+                <TabPanel>
+                  <CommonCardLayout props={data.allGraphCmsEvent.nodes} />
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          </Box>
+        </Box>
+      )}
+    />
   );
 }
 
