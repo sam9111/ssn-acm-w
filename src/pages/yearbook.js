297c205a27e51581b6ca11bcddf3328deb6e3f82
sam9111 <samyuktha19095@cse.ssn.edu.in>
2021-09-02 15:17:39 +0530

    year dropdown working

--- src/pages/yearbook.js
+++ src/pages/yearbook.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Heading,
   Tabs,
@@ -14,78 +14,121 @@ import {
 } from "@chakra-ui/react";
 
 import CommonCardLayout from "../components/CommonCardLayout";
+import { graphql, StaticQuery } from "gatsby";
 
 import TeamGrid from "../components/TeamGrid";
 
+function generate_years() {
+  const years = [];
+  const yearNow = new Date().getFullYear();
+  for (var i = 0; i < yearNow - 2018 + 1; i++) {
+    years.push(yearNow - i);
+  }
+  return years;
+}
+const YEARS = generate_years().reverse();
+
 function YearBookPage() {
+  const [year, setYear] = useState(YEARS[0]);
+  console.log(year);
   return (
-    <Box>
-      <Stack
-        py={{ base: 10, md: 14 }}
-        align={"center"}
-        spacing={{ base: 8, md: 10 }}
-      >
-        <Heading
-          lineHeight={1.1}
-          fontWeight={600}
-          fontSize={{ base: "3xl", sm: "4xl", lg: "5xl" }}
-        >
-          <Text as={"span"} position={"relative"}>
-            Yearbook
-          </Text>
-        </Heading>
-        <Box
-          w="xs"
-          bg={useColorModeValue("white", "gray.800")}
-          fontSize="xl"
-          borderRadius={"3xl"}
-        >
-          <Select placeholder="Select option" size="lg" borderRadius={"3xl"}>
-            <option value="option1">Option 1</option>
-            <option value="option2">Option 2</option>
-            <option value="option3">Option 3</option>
-          </Select>
-        </Box>
-        <Box
-          bg={useColorModeValue("white", "gray.800")}
-          boxShadow="2xl"
-          borderRadius={"3xl"}
-        >
-          <Tabs
-            variant="enclosed"
-            align="center"
-            isFitted
-            width={"full"}
-            borderRadius={"3xl"}
+    <StaticQuery
+      query={graphql`
+        query {
+          allGraphCmsEvent(filter: { stage: { eq: PUBLISHED } }) {
+            nodes {
+              title
+              about
+              when
+              coverImage {
+                url
+              }
+              timeCategory
+            }
+          }
+        }
+      `}
+      render={(data) => (
+        <Box>
+          <Stack
+            py={{ base: 10, md: 14 }}
+            align={"center"}
+            spacing={{ base: 8, md: 10 }}
           >
-            <TabList>
-              <Tab>
-                <Text
-                  fontSize={{ base: "lg", sm: "xl", lg: "2xl" }}
-                  fontWeight="bold"
-                >
-                  Events
-                </Text>
-              </Tab>
-              <Tab>
-                <Text
-                  fontSize={{ base: "lg", sm: "xl", lg: "2xl" }}
-                  fontWeight="bold"
-                >
-                  Team
-                </Text>
-              </Tab>
-            </TabList>
-            <TabPanels>
-              <TabPanel></TabPanel>
-              <TabPanel>
-                <TeamGrid team={[]} faculty={[]} />
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
+            <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: "3xl", sm: "4xl", lg: "5xl" }}
+            >
+              <Text as={"span"} position={"relative"}>
+                Yearbook
+              </Text>
+            </Heading>
+            <Box
+              w="xs"
+              bg={useColorModeValue("white", "gray.800")}
+              fontSize="xl"
+              borderRadius={"3xl"}
+            >
+              <Select
+                placeholder="Select option"
+                size="lg"
+                borderRadius={"3xl"}
+                value={year}
+                onChange={(e) => setYear(e.target.value)}
+              >
+                {YEARS.map((year) => (
+                  <option value={year}>{year + "-" + (year + 1)}</option>
+                ))}
+              </Select>
+            </Box>
+            <Box boxShadow="md" borderRadius={"3xl"}>
+              <Tabs
+                variant="enclosed"
+                align="center"
+                isFitted
+                width={"full"}
+                borderRadius={"3xl"}
+              >
+                <TabList>
+                  <Tab>
+                    <Text
+                      fontSize={{ base: "lg", sm: "xl", lg: "2xl" }}
+                      fontWeight="bold"
+                    >
+                      Events
+                    </Text>
+                  </Tab>
+                  <Tab>
+                    <Text
+                      fontSize={{ base: "lg", sm: "xl", lg: "2xl" }}
+                      fontWeight="bold"
+                    >
+                      Team
+                    </Text>
+                  </Tab>
+                </TabList>
+                <TabPanels p={10}>
+                  <TabPanel>
+                    {" "}
+                    <CommonCardLayout
+                      events={data.allGraphCmsEvent.nodes.filter((event) => {
+                        var d = new Date(event.when);
+                        var y = d.getUTCFullYear();
+                        return y == year;
+                      })}
+                    />
+                  </TabPanel>
+                  <TabPanel>
+                    <TeamGrid team={[]} faculty={[]} />
+                  </TabPanel>
+                </TabPanels>
+              </Tabs>
+            </Box>
+          </Stack>
         </Box>
-      </Stack>
-    </Box>
+      )}
+    />
   );
 }
 
