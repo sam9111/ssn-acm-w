25b84a557fc34353be5b4017fe231498b57c2dfe
ramumaha <62795822+ramumaha@users.noreply.github.com>
2021-09-02 12:28:50 +0530

    Added the graphql part (#22)
    * Added the graphql part
    
    Doesnt work as expected
    
    * Update CommonCardLayout.js

--- src/components/CommonCardLayout.js
+++ src/components/CommonCardLayout.js
@@ -1,18 +1,25 @@
 import React from "react";
-import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, useProps } from "@chakra-ui/react";
 import careertalk from "../images/careertalk.png";
 import CommonCard from "../components/CommonCard.js";
 
-export default function CommonCardLayout() {
+export default function CommonCardLayout(props) {
+  // const entries = Object.entries(props);
+  const value = Object.values(props);
+  console.log(value);
+  const temp = value[0];
+
   return (
     <SimpleGrid spacing="40px" columns={{ base: 1, md: 2 }} width={"full"}>
-      <CommonCard
-        title=" Coding Essentials"
-        imageImport={careertalk}
-        body="Workshop on essentviewsdfjs  "
-        link="Check it Out!"
-        cardType="events"
-      />
+      {temp.map((eventElement) => (
+        <CommonCard
+          title={eventElement.title}
+          imageImport={careertalk}
+          body={eventElement.about}
+          link="www.google.com"
+          cardType="event"
+        />
+      ))}
     </SimpleGrid>
   );
 }
